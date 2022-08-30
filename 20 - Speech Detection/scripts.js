a7a70833c3ef5b7ca992f04f3962c4f24476aa59
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-08-30 14:46:59 -0300

    added npm modules
    I'm trying to use lodash, but something is wrong

---
+++ 20 - Speech Detection/scripts.js
@@ -0,0 +1,35 @@
+import { debounce } from 'lodash';
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+function gettingTheTime() {
+  timeNow = new Date();
+  p.innerHTML = `<p style="color:#FF0000"> >> ${timeNow}</p>`
+}
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+
+    p.textContent = transcript;
+    if(e.results[0].isFinal) {
+      p = document.createElement('p');
+      words.appendChild(p);
+  }
+
+  if(transcript.includes('horas')) {
+    debounce(gettingTheTime, 0);
+  }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
